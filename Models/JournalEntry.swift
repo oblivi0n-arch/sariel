e7ed3f8588d907c95ce3af829cf5344b60de2c15
oblivi0n-arch <jakub.deck8@gmail.com>
2026-07-15 20:18:46 +0200

    feat: add JournalEntry SwiftData model

---
+++ Models/JournalEntry.swift
@@ -0,0 +1,38 @@
+import Foundation
+import SwiftData
+
+@Model
+final class JournalEntry {
+    var id: UUID
+    var title: String
+    var content: String
+    var createdAt: Date
+    var mood: String
+
+    init(title: String = "New entry", content: String = "", mood: Mood = .neutral) {
+        self.id = UUID()
+        self.title = title
+        self.content = content
+        self.createdAt = Date()
+        self.mood = mood.rawValue
+    }
+
+    var entryMood: Mood {
+        get { Mood(rawValue: mood) ?? .neutral }
+        set { mood = newValue.rawValue }
+    }
+}
+
+enum Mood: String, CaseIterable {
+    case great, good, neutral, bad, awful
+
+    var symbolName: String {
+        switch self {
+        case .great:   "sun.max"
+        case .good:    "cloud.sun"
+        case .neutral: "cloud"
+        case .bad:     "cloud.rain"
+        case .awful:   "cloud.bolt.rain"
+        }
+    }
+}
