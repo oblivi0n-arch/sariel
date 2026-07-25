d071eeb1b3c6324efe606abe903407d82e75ee7f
oblivi0n-arch <jakub.deck8@gmail.com>
2026-07-25 20:20:07 +0200

    feat(achievements): register AchievementUnlock in ModelContainer schema

--- sariel/sarielApp.swift
+++ sariel/sarielApp.swift
@@ -22,7 +22,7 @@ struct SarielApp: App {
 
     init() {
         do {
-            container = try ModelContainer(for: Conversation.self, ChatMessage.self, JournalEntry.self, JournalEntryTag.self, Commitment.self)
+            container = try ModelContainer(for: Conversation.self, ChatMessage.self, JournalEntry.self, JournalEntryTag.self, Commitment.self, AchievementUnlock.self)
         } catch {
             fatalError("Cannot initialize container: \(error)")
         }
